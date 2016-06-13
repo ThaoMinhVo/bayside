b436a77f725e76cffd0efaf402fd3f61e845563b
Thao Vo <TvO@Thaos-MacBook-Pro.local>
2016-06-13 10:03:54 -0700

    Added useful comments and reminders. Also added prefixfree javascript file to handle cross browser support.

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,12 @@
+//makes sure function excuted only after page finish loading 
+//and protects variables to prevent them from being global variables
 $(function(){
     
+    //Handles layout while resizing window because 
+    //no scroll action takes place to call scroll function
     $(window).resize(function () {
         $("header div#topNav-1").css('display', 'block');
-        if($(window).width() <= 767){
+        if($(window).width() <= 767){ // <768 to accomodate for scrollbar
             $("header nav").css('margin-top', '187px');
         }else{
             $("header nav").css('margin-top', '129px');
@@ -13,6 +17,7 @@ $(function(){
     var bluebg = $('#bluebg');
     var offset = bluebg.offset();
 
+    //handles appearance of logo and social nav depending on location of page scroll
     if (bluebg.length){
         $(document).scroll(function() { 
             scroll_start = $(this).scrollTop()+129;
